42dcc3337a604d4a4c2e334c23b128b658e800ac
Quin Jacobs <quinjaco0@gmail.com>
2025-06-11 05:19:45 -0600

    Fix: Stripe button not working

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -1,11 +1,11 @@
-// src/utils/stripe.js
+// /src/utils/stripe.js
 import { loadStripe } from '@stripe/stripe-js';
 
 let stripePromise;
 
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe('pk_test_51RTYhiPZu2cKz2PwoGYw2uJ1KfbUGK2OjMc2unsyKB4G9KybbEMzpQoN7PYm6d65RfsxwrTSkpOZhGScWeeNzf5m00krLb3w2b');
+    stripePromise = loadStripe('pk_live_51RTYhaB3KzN8XjUWtf9lVAO1CFKjPtjqb7juBDMTRaiJA7oJRmfDjVZPNDdyah095hrXMKI4wabf6NLJJEtliaIr00yVRD0bzw'); // your LIVE public key here
   }
   return stripePromise;
 };
